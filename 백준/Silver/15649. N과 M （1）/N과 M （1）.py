2b8842c7e6af10b004a3c2a2a846080529e0865f
chjung99 <62923434+chjung99@users.noreply.github.com>
2023-10-26 20:36:17 +0900

    [Silver III] Title: N과 M (1), Time: 244 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,24 +1,25 @@
 # 순열 nPr
-ret = []
+import sys
+
+
+# def input():
+#     return sys.stdin.readline().rstrip()
+
+
 n, m = map(int, input().split())
+choose = [0 for _ in range(10)]
+used = [0 for _ in range(10)]
 
-
-def func(v):
-    global ret
-    if len(v) == m:
-        for vv in v:
-            print(vv, end=" ")
-        print("")
-        return
-    for j in range(1, n+1):
-        if j not in v:
-            v.append(j)
-            func(v)
-            v.pop()
-
-for i in range(1, n+1):
-    func([i])
-for vv in ret:
-    for v in vv:
-        print(v, end=" ")
-    print("")
+def dfs(cnt):
+    if cnt == m:
+        for idx in range(cnt):
+            print(choose[idx], end=' ')
+        print()
+    for i in range(1, n+1):
+        if not used[i]:
+            used[i] = 1
+            choose[cnt] = i
+            dfs(cnt+1)
+            choose[cnt] = 0
+            used[i] = 0
+dfs(0)
