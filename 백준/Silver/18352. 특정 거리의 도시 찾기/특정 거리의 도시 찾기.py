c9f3f0cdf499cc1b7e4f3ff3d31392fefa2247a1
chjung99 <62923434+chjung99@users.noreply.github.com>
2023-12-25 20:29:48 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 3208 ms, Memory: 179444 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -0,0 +1,36 @@
+import heapq
+import sys
+input = sys.stdin.readline
+N, M, K, X = map(int, input().split())
+graph = [[] for _ in range(N + 1)]
+dist = [float("inf") for _ in range(N + 1)]
+
+for i in range(M):
+    A, B = map(int, input().split())
+    graph[A].append((1, B))
+
+
+def dijstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    dist[start] = 0
+    while q:
+        cur_dist, cur = heapq.heappop(q)
+
+        if cur_dist > dist[cur]:
+            continue
+
+        for nxt_dist, nxt in graph[cur]:
+            if dist[nxt] > cur_dist + nxt_dist:
+                dist[nxt] = cur_dist + nxt_dist
+                heapq.heappush(q, (dist[nxt], nxt))
+
+
+dijstra(X)
+
+if dist.count(K) == 0:
+    print("-1")
+else:
+    for i in range(1, N+1):
+        if dist[i] == K:
+            print(i)
