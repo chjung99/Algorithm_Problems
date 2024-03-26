333a912acf2c022b2187d24d6469857e5cac042d
chjung99 <62923434+chjung99@users.noreply.github.com>
2024-03-26 10:28:32 +0900

    [Silver III] Title: 조합, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2407. 조합/조합.py
+++ 백준/Silver/2407. 조합/조합.py
@@ -1,5 +1,12 @@
-import math
-
 n, m = map(int, input().split())
 
-print(math.comb(n, m))
+def factorial(n):
+    res = 1
+    for i in range(1, n+1):
+        res *= i
+    return res
+def comb(n, m):
+    return factorial(n)//(factorial(m)*factorial(n-m))
+
+
+print(comb(n, m))
