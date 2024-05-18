94f6bd1e75abecf51bc66f4ab90d2c4046f9f0e9
chjung99 <62923434+chjung99@users.noreply.github.com>
2024-05-18 16:44:13 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.58 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -0,0 +1,29 @@
+def out_of_range(x, y, n):
+    return x < 0 or x >= n or \
+        y < 0 or y >= n
+
+def solution(n):
+    arr = [[0 for _ in range(n)]for __ in range(n)]
+    visit = [[0 for _ in range(n)]for __ in range(n)]
+    dx = [0, 1, 0, -1]
+    dy = [1, 0, -1, 0]
+    cx, cy, cd = 0, 0, 0
+    cnt = 1
+    visit[cx][cy] = 1
+    arr[cx][cy] = cnt
+    T = n ** 2
+    
+    while cnt < T:
+        cnt += 1
+        
+        nx, ny = cx + dx[cd], cy + dy[cd]
+        if out_of_range(nx, ny, n) or visit[nx][ny]:
+            cd = (cd + 1) % 4
+            nx, ny = cx + dx[cd], cy + dy[cd]
+            
+        arr[nx][ny] = cnt
+        visit[nx][ny] = 1
+        
+        cx, cy = nx, ny
+    
+    return arr
