c29c0c63892cdf0463afe7942ae4181d28ace195
chjung99 <62923434+chjung99@users.noreply.github.com>
2024-09-23 21:47:56 +0900

    [Silver III] Title: 합 구하기, Time: 2800 ms, Memory: 121628 KB -BaekjoonHub

--- 백준/Silver/11441. 합 구하기/합 구하기.py
+++ 백준/Silver/11441. 합 구하기/합 구하기.py
@@ -1,11 +1,10 @@
 N = int(input())
-arr = list(map(int ,input().split()))
+arr = list(map(int, input().split()))
 M = int(input())
-dp = [0 for _ in range(N+1)]
-
+acc_sum = [0 for _ in range(N+1)]
 for i in range(1, N+1):
-    dp[i] += arr[i-1] + dp[i-1]
+    acc_sum[i] = acc_sum[i-1] + arr[i-1]
 
 for _ in range(M):
-    s, e = map(int, input().split())
-    print(dp[e]-dp[s-1])
+    i, j = map(int, input().split())
+    print(acc_sum[j]-acc_sum[i-1])
