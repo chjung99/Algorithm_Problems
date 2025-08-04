186c59e2988f4de24e60dd55b8c4b939f564f5b1
chjung99 <62923434+chjung99@users.noreply.github.com>
2025-08-04 20:19:39 +0900

    [Bronze III] Title: 완전 세제곱, Time: 300 ms, Memory: 16400 KB -BaekjoonHub

---
+++ 백준/Bronze/4690. 완전 세제곱/완전 세제곱.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int a=2; a<=100; a++){
+            for (int b=2; b<=100; b++){
+                for (int c=2; c<=100; c++){
+                    for (int d=2; d<=100; d++){
+                        if (b<=c && c<= d) {
+                            findTriple(a, b, c, d);
+                        }
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    private static void findTriple(int a, int b, int c, int d) {
+        if (a*a*a == (b*b*b + c*c*c + d*d*d)){
+            System.out.println("Cube = "+a+", Triple = ("+b+","+c+","+d+")");
+        }
+        return;
+    }
+}
