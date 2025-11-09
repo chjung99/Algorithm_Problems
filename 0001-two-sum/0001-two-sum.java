6a535daf2e93cd8ce13c52895d59b74e99ce0f1f
정찬호 <62923434+chjung99@users.noreply.github.com>
2025-11-09 14:38:00 +0900

    Time: 11 ms (46.97%), Space: 47.9 MB (5.22%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,45 @@
 import java.util.*;
 
 class Solution {
+
     public int[] twoSum(int[] nums, int target) {
-        
+        int[] answer = new int[2];
+        Map<Integer, ArrayDeque<Integer>> hashMap = new HashMap<>();
+
         for (int i=0; i < nums.length; i++){
-            for (int j=i+1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
+            int key = nums[i];
+            if (hashMap.containsKey(key)){
+                ArrayDeque<Integer> bucket =  hashMap.get(key);
+                bucket.push(i);
+                hashMap.put(nums[i], bucket);
+            } else {
+                hashMap.put(nums[i], new ArrayDeque<>(List.of(i)));
+            }
+        }
+
+        for (int i=0; i < nums.length ; i++){
+            int key = target - nums[i];
+            
+            if (hashMap.containsKey(key)){
+                ArrayDeque<Integer> bucket = hashMap.get(key);
+                answer[0] = i;
+                if (key == nums[i]){
+                    while (!bucket.isEmpty()){
+                        answer[1] = bucket.pop();
+                        if (answer[1] != i){
+                            break;
+                        }
+                    }
+                    if (answer[0] != answer[1]) break;
+                     
+                } else {
+                    answer[1] = bucket.pop();
+                    break;
+
                 }
             }
         }
-        return new int[]{-1, -1};
+        
+        return answer;
     }
 }
