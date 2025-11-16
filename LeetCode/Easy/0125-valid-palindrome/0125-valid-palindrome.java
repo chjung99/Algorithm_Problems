6a79cc54824efdaa97f9e5b7e9702e4a32e87bdc
정찬호 <62923434+chjung99@users.noreply.github.com>
2025-11-16 16:59:31 +0900

    Time: 6 ms (42.14%), Space: 46.8 MB (5.36%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,17 +2,16 @@ class Solution {
     public boolean isPalindrome(String s) {
         String filterd = s.toLowerCase();
         StringBuilder filterdBuilder = new StringBuilder();
+
         for (int i = 0; i < filterd.length(); i++){
             Character c = filterd.charAt(i);
-            if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')||(c >= '0' && c <= '9')){
+            if (Character.isLetterOrDigit(c)){
                 filterdBuilder.append(c.toString());
             }
         }
+
         String ori = filterdBuilder.toString();
         String rev = new StringBuilder(ori).reverse().toString();
-
-        System.out.println(ori);
-        System.out.println(rev);
         
         return rev.equals(ori);
     }
