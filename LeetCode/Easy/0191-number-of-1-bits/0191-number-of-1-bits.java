575ae31056fb36a52723a26e90d83749706e5bca
정찬호 <62923434+chjung99@users.noreply.github.com>
2025-11-22 17:36:53 +0900

    Time: 1 ms (18.72%), Space: 42.7 MB (5.14%) - LeetHub

---
+++ LeetCode/Easy/0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int hammingWeight(int n) {
+        int cnt = 0;
+        while (n != 0){
+            if (n % 2 == 1) cnt ++;
+            n = (int) (n/2);
+        }
+        return cnt;
+    }
+}
