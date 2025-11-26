c889f324a387233609a86252c2453127a5886c0c
정찬호 <62923434+chjung99@users.noreply.github.com>
2025-11-26 20:32:32 +0900

    Time: 6 ms (43.89%), Space: 44 MB (69.96%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
@@ -2,16 +2,15 @@ class Solution {
     int SIZE = 26;
     public boolean isAnagram(String s, String t) {
         int[] countS = new int[SIZE];
-        int[] countT = new int[SIZE];
 
         if (s.length() != t.length()) return false;
         
         for (int i = 0; i < s.length(); i++){
             countS[s.charAt(i)-'a'] += 1;
-            countT[t.charAt(i)-'a'] += 1;
+            countS[t.charAt(i)-'a'] -= 1;
         }
         for (int i = 0; i < SIZE; i++){
-            if (countS[i] != countT[i]) return false;
+            if (countS[i] != 0) return false;
         }
         return true;
     }
