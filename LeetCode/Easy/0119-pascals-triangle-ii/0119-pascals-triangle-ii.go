176b116281c6ffd3251f9c36cc7fffb652391d81
정찬호 <62923434+chjung99@users.noreply.github.com>
2026-02-22 16:44:46 +0900

    Time: 0 ms (100%), Space: 4.1 MB (18.18%) - LeetHub

---
+++ LeetCode/Easy/0119-pascals-triangle-ii/0119-pascals-triangle-ii.go
@@ -0,0 +1,19 @@
+func getRow(rowIndex int) []int {
+    arr := generate(rowIndex+1)
+    return arr[rowIndex]
+}
+
+func generate(numRows int) [][]int {
+    arr := make([][]int, numRows)
+    for i := 0; i < numRows; i++ {
+        arr[i] = make([]int, i+1)
+        for j := 0; j < i+1; j++ {
+            if (j == 0 || j == i) {
+                arr[i][j] = 1
+            } else {
+                arr[i][j] = arr[i-1][j-1] + arr[i-1][j]
+            }
+        }
+    }
+    return arr
+}
