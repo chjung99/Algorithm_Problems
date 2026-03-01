673389ae3192007f18ee2c0478f0fe04a625783e
정찬호 <62923434+chjung99@users.noreply.github.com>
2026-03-01 12:56:46 +0900

    Time: 0 ms (100%), Space: 5.8 MB (44.21%) - LeetHub

---
+++ LeetCode/Easy/0412-fizz-buzz/0412-fizz-buzz.go
@@ -0,0 +1,15 @@
+func fizzBuzz(n int) []string {
+    var ret []string
+    for i := 1; i <= n; i++ {
+        if (i % 3 == 0 && i % 5 == 0){
+            ret = append(ret, "FizzBuzz")
+        } else if (i % 3 == 0) {
+            ret = append(ret, "Fizz")
+        } else if (i % 5 == 0) {
+            ret = append(ret, "Buzz")
+        } else {
+            ret = append(ret, strconv.Itoa(i))
+        }
+    }
+    return ret
+}
