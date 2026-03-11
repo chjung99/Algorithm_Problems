99600cf77bf36e136c1656e7b00197ec0298c85f
Chanho Jung <62923434+chjung99@users.noreply.github.com>
2026-03-11 20:12:22 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0263-ugly-number/0263-ugly-number.go
+++ LeetCode/Easy/0263-ugly-number/0263-ugly-number.go
@@ -4,9 +4,7 @@ func isUgly(n int) bool {
     }
     for i := 2; (i*i) <= n; i++ {
         for (n % i == 0) {
-            fmt.Println(i)
             if (i != 2 && i != 3 && i != 5) {
-                fmt.Println(i)
                 return false
             }
             n = n / i
@@ -14,7 +12,6 @@ func isUgly(n int) bool {
     }
     if (n > 1) {
         if (n != 2 && n != 3 && n != 5) {
-            fmt.Println(n)
             return false
         }
     }
