1cf0afb420ccb24fd003d37d91e3926d0b898d03
Chanho Jung <62923434+chjung99@users.noreply.github.com>
2026-03-18 21:38:25 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0383-ransom-note/0383-ransom-note.go
+++ LeetCode/Easy/0383-ransom-note/0383-ransom-note.go
@@ -1,34 +1,15 @@
 func canConstruct(ransomNote string, magazine string) bool {
-    x := make(map[rune]int)
-    y := make(map[rune]int)
+    counts := make(map[rune]int)
 
-    for _, val := range(ransomNote) {
-        cnt, exists :=  x[val]
-
-        if (!exists) {
-            x[val] = 0
-        } else {
-            x[val] = cnt + 1
-        }
+    for _, char := range magazine {
+        counts[char]++
     }
 
-    for _, val := range(magazine) {
-        cnt, exists :=  y[val]
-
-        if (!exists) {
-            y[val] = 0
-        } else {
-            y[val] = cnt + 1
-        }
-    }
-
-    for key, cntX := range x {
-        cntY, exists := y[key]
-        if (!exists) {
-            return false
-        } else if (cntY < cntX){
+    for _, char := range ransomNote {
+        if (counts[char] == 0) {
             return false
         }
+        counts[char]--
     }
     return true
 }
