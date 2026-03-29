57385bc3945071890d6bb3284db89995461e8c9e
Chanho Jung <62923434+chjung99@users.noreply.github.com>
2026-03-29 12:47:42 +0900

    Time: 1 ms (42.34%), Space: 61 MB (90.13%) - LeetHub

--- LeetCode/Medium/0189-rotate-array/0189-rotate-array.java
+++ LeetCode/Medium/0189-rotate-array/0189-rotate-array.java
@@ -1,6 +1,9 @@
 class Solution {
     public void rotate(int[] nums, int k) {
         int n = nums.length;
+        if (k > n) {
+            k = k % n;
+        }
         reverse(nums, 0, n-1);
         reverse(nums, 0, k-1);
         reverse(nums, k, n-1);
