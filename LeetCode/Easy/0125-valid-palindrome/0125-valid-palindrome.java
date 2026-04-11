acb43ac52b8bc35cb4df6ee664c2b7fd29c688bf
Chanho Jung <62923434+chjung99@users.noreply.github.com>
2026-04-11 14:06:51 +0900

    Time: 4 ms (48.49%), Space: 44.9 MB (45.2%) - LeetHub

--- LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
+++ LeetCode/Easy/0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,18 +1,26 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String filterd = s.toLowerCase();
-        StringBuilder filterdBuilder = new StringBuilder();
+        s = s.toLowerCase();
+        StringBuilder sb =  new StringBuilder();
 
-        for (int i = 0; i < filterd.length(); i++){
-            Character c = filterd.charAt(i);
-            if (Character.isLetterOrDigit(c)){
-                filterdBuilder.append(c.toString());
+        for (int i = 0; i < s.length(); i++) {
+            if ((s.charAt(i) >= 'a' && s.charAt(i) <= 'z') 
+            || (s.charAt(i) >= '0' && s.charAt(i) <= '9')){
+                sb.append(s.charAt(i));
             }
         }
 
-        String ori = filterdBuilder.toString();
-        String rev = new StringBuilder(ori).reverse().toString();
-        
-        return rev.equals(ori);
+        String conv = sb.toString();
+        int left = 0;
+        int right = conv.length()-1;
+
+        while (left < right) {
+            if (conv.charAt(left) != conv.charAt(right)) {
+                return false;
+            }
+            left += 1;
+            right -= 1;
+        }
+        return true;
     }
 }
