eedf46c96b8f2ee85f9b926f56c062ffd0c67c1c
Chanho Jung <62923434+chjung99@users.noreply.github.com>
2026-04-16 17:20:38 +0900

    Time: 3 ms (92.71%), Space: 44.3 MB (76.66%) - LeetHub

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,26 @@
 class Solution {
-    static public boolean isAnagram(String s, String t) {
-        int[] countS = new int[26];
-        int slen = s.length();
-        int tlen = t.length();
+    public boolean isAnagram(String s, String t) {
+        int SIZE = 26;
+        int[] cnt = new int[SIZE];
+        int n = s.length();
+        int m = t.length();
 
-        if (slen != tlen) return false;
-        
-        for (int i = 0; i < slen; i++){
-            countS[s.charAt(i)-'a'] += 1;
-            countS[t.charAt(i)-'a'] -= 1;
+        if (n != m) {
+            return false;
         }
-        for (int i = 0; i < 26; i++){
-            if (countS[i] != 0) return false;
+
+        for (int i = 0; i < n; i++) {
+            cnt[s.charAt(i)-'a'] += 1;
         }
+
+        for (int i = 0; i < m; i++) {
+            if (cnt[t.charAt(i)-'a'] > 0) {
+                cnt[t.charAt(i)-'a'] -= 1;
+            } else {
+                return false;
+            }
+        }
+
         return true;
     }
 }
