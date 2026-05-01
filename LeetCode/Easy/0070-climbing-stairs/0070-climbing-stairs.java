e48b0f80f23297ecad1fbfc3f901e3b8e9878326
Chanho Jung <62923434+chjung99@users.noreply.github.com>
2026-05-01 12:50:04 +0900

    Time: 0 ms (100%), Space: 42.4 MB (17.17%) - LeetHub

--- LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
+++ LeetCode/Easy/0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,12 +1,10 @@
 class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[46];
-        dp[0] = 1;
         dp[1] = 1;
-        dp[2] = dp[1] + dp[0];
-        dp[3] = dp[2] + dp[1];
+        dp[2] = dp[1] + 1;
 
-        for (int i = 4; i <= n; i++) {
+        for (int i = 3; i <= n; i++) {
             dp[i] = dp[i-1] + dp[i-2];
         }
         return dp[n];
