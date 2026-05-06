59e19ae53eccee325c219edea7847e50ce4cdc87
Chanho Jung <62923434+chjung99@users.noreply.github.com>
2026-05-06 20:12:31 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- LeetCode/Easy/0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
+++ LeetCode/Easy/0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -14,34 +14,22 @@
  * }
  */
 class Solution {
+    int minDiff = Integer.MAX_VALUE;
+    Integer prev = null;
     public int getMinimumDifference(TreeNode root) {
-        List<Integer> nums = new ArrayList<>();
-
-        BFS(root, nums);
-        nums.sort(Comparator.naturalOrder());
-        System.out.print(nums);
-        int ans = nums.get(1) - nums.get(0);
-        
-        for (int i = 0; i < nums.size() - 1; i++) {
-            ans = Math.min(ans, nums.get(i+1)-nums.get(i));
-        }
-        return ans;
+        inorder(root);
+        return minDiff;
     }
-    public void BFS(TreeNode node, List<Integer> nums) {
-        Deque<TreeNode> q = new ArrayDeque<>();
-        q.add(node);
-        nums.add(node.val);
-
-        while (!q.isEmpty()) {
-            TreeNode cur = q.removeFirst();
-            if (cur.left != null) {
-                nums.add(cur.left.val);
-                q.add(cur.left);
-            }
-            if (cur.right != null) {
-                nums.add(cur.right.val);
-                q.add(cur.right);
-            }
+    public void inorder(TreeNode node) {
+        if (node == null) {
+            return;
         }
+        inorder(node.left);
+        if (prev == null) {
+            prev = node.val;
+        } else {
+            minDiff = Math.min(minDiff, node.val - prev);
+        }
+        inorder(node.right);
     }
 }
