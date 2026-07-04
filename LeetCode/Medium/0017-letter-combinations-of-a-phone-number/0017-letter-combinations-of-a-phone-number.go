8f78e5aab3a75b572b353498d41da1af6c21be79
Chanho Jung <62923434+chjung99@users.noreply.github.com>
2026-07-04 13:46:41 +0900

    Time: 0 ms (100%), Space: 4 MB (84.44%) - LeetHub

---
+++ LeetCode/Medium/0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.go
@@ -0,0 +1,30 @@
+func letterCombinations(digits string) []string {
+    ret := make([]string, 0)
+    m := make([][]byte, 10)
+    m[0] = make([]byte, 0)
+    m[1] = make([]byte, 0)
+    m[2] = []byte{'a', 'b', 'c'}
+    m[3] = []byte{'d', 'e', 'f'}
+    m[4] = []byte{'g', 'h', 'i'}
+    m[5] = []byte{'j', 'k', 'l'}
+    m[6] = []byte{'m', 'n', 'o'}
+    m[7] = []byte{'p', 'q', 'r', 's'}
+    m[8] = []byte{'t', 'u', 'v'}
+    m[9] = []byte{'w', 'x', 'y', 'z'}
+
+    dfs(digits, m, &ret, 0, len(digits), "")
+
+    return ret
+}
+
+func dfs(digits string, m [][]byte, ret *[]string, depth int, n int, out string){
+    if (depth == n) {
+        fmt.Println(out)
+        *ret = append(*ret, out)
+        return
+    }
+    idx := int(digits[depth] - '0')
+    for _, char := range m[idx] {
+        dfs(digits, m, ret, depth + 1, n, out + string(char))
+    }
+}
